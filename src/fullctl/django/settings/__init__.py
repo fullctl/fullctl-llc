49a3faf7b2ec886951097983bb02c599019c89b1
Matt Griswold <grizz@20c.com>
2021-10-21 18:19:19 +0000

    add default settings

--- src/fullctl/django/settings/__init__.py
+++ src/fullctl/django/settings/__init__.py
@@ -1,19 +1,18 @@
+"""
+This file handles the default django settings for fullctl services.
+
+Versioning has been dropped in favor of using separate function names.
+"""
 import os
 import sys
 
 import confu.util
 
 
-# TODO : add dict access and logging
-class SettingsManager(confu.util.SettingsManager):
-    def print_debug(self, *args, **kwargs):
-        if DEBUG:
-            print(*args, **kwargs)
-
 
 def print_debug(*args, **kwargs):
-    if DEBUG:
-        print(*args, **kwargs)
+    # XXX
+    print(*args, **kwargs)
 
 
 def get_locale_name(code):
@@ -29,22 +28,52 @@ def get_locale_name(code):
     return language_map.get(language, code)
 
 
-def try_include(filename):
-    """Tries to include another file from the settings directory."""
-    print_debug(f"including {filename} {RELEASE_ENV}")
-    try:
-        with open(filename) as f:
-            exec(compile(f.read(), filename, "exec"), globals())
-
-        print_debug(f"loaded additional settings file '{filename}'")
-
-    except FileNotFoundError:
-        print_debug(f"additional settings file '{filename}' was not found, skipping")
 
 
 def read_file(name):
     with open(name) as fh:
         return fh.read()
+
+
+# TODO : add dict access and logging
+class SettingsManager(confu.util.SettingsManager):
+
+    # settings manager extensions
+
+    def get(self, name):
+        """Get name, raise if not set.
+            Should use _DEFAULT_ARG
+        """
+#        if key in self.scope:
+        return self.scope[name]
+
+    def set_option(self, name, value, envvar_type=None):
+        """ Return the resulting value after setting."""
+        super().set_option(name, value, envvar_type)
+        return self.get(name)
+
+    def print_debug(self, *args, **kwargs):
+        if DEBUG:
+            print(*args, **kwargs)
+
+    def try_include(self, filename):
+        """Tries to include another file into current scope."""
+        print_debug(f"including {filename}")
+        try:
+            with open(filename) as f:
+                exec(compile(f.read(), filename, "exec"), self.scope)
+    
+            print_debug(f"loaded additional settings file '{filename}'")
+    
+        except FileNotFoundError:
+            print_debug(f"additional settings file '{filename}' was not found, skipping")
+
+    def try_include_env(self, suffix=""):
+        # look for mainsite/settings/${RELEASE_ENV}.py and load if it exists
+        # needs __file__ from caller
+        env_file = os.path.join(os.path.dirname(__file__), f"{self.get('RELEASE_ENV')}.py")
+        settings.try_include(env_file)
+
 
 
 def set_release_env_v1(settings_manager):
@@ -134,3 +163,125 @@ def set_default_v1(settings_manager):
 
     settings_manager.set_default("ROOT_URLCONF", f"{service_tag}.urls")
     settings_manager.set_default("WSGI_APPLICATION", f"{service_tag}.wsgi.application")
+
+    # eval from default.py file
+    filename = os.path.join(os.path.dirname(__file__), f"default.py")
+    settings_manager.try_include(filename)
+
+
+def set_default_append(settings_manager):
+    DEBUG = settings_manager.get("DEBUG")
+    settings_manager.set_option("DEBUG_EMAIL", DEBUG)
+    for template in settings_manager.get("TEMPLATES"):
+        template["OPTIONS"]["debug"] = DEBUG
+    #TEMPLATES[0]["OPTIONS"]["debug"] = DEBUG
+
+
+    # use structlog for logging
+    import structlog
+    MIDDLEWARE = settings_manager.get("MIDDLEWARE")
+    
+    MIDDLEWARE += [
+        "django_structlog.middlewares.RequestMiddleware",
+    ]
+    
+    # set these explicitly, not with DEBUG
+    DJANGO_LOG_LEVEL = settings_manager.set_option("DJANGO_LOG_LEVEL", "INFO")
+    FULLCTL_LOG_LEVEL = settings_manager.set_option("FULLCTL_LOG_LEVEL", "DEBUG")
+    
+    structlog.configure(
+        processors=[
+            structlog.stdlib.filter_by_level,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.stdlib.add_logger_name,
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.PositionalArgumentsFormatter(),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            structlog.processors.UnicodeDecoder(),
+            structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+        ],
+        context_class=structlog.threadlocal.wrap_dict(dict),
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        wrapper_class=structlog.stdlib.BoundLogger,
+        cache_logger_on_first_use=True,
+    )
+    
+    # logging define extra formatters and handlers for convenience
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "json": {
+                "()": structlog.stdlib.ProcessorFormatter,
+                "processor": structlog.processors.JSONRenderer(),
+            },
+            "color_console": {
+                "()": structlog.stdlib.ProcessorFormatter,
+                "processor": structlog.dev.ConsoleRenderer(),
+            },
+            "key_value": {
+                "()": structlog.stdlib.ProcessorFormatter,
+                "processor": structlog.processors.KeyValueRenderer(
+                    key_order=["timestamp", "level", "event", "logger"]
+                ),
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "color_console",
+                "stream": sys.stdout,
+            },
+            "console_json": {
+                "class": "logging.StreamHandler",
+                "formatter": "json",
+                "stream": sys.stdout,
+            },
+            "mail_admins": {
+                "class": "django.utils.log.AdminEmailHandler",
+                "level": "ERROR",
+                # plain text by default - HTML is nicer
+                "include_html": True,
+            },
+        },
+        "loggers": {
+            "django": {
+                "handlers": ["console_json"],
+                "level": DJANGO_LOG_LEVEL,
+            },
+            "django_structlog": {
+                "handlers": ["console_json"],
+                "level": FULLCTL_LOG_LEVEL,
+            },
+        },
+    }
+
+
+def set_languages_docs(settings_manager):
+    settings_manager.set_option("ENABLE_ALL_LANGUAGES", False)
+
+    if ENABLE_ALL_LANGUAGES:
+        language_dict = dict(LANGUAGES)
+        for locale_path in LOCALE_PATHS:
+            for name in os.listdir(locale_path):
+                path = os.path.join(locale_path, name)
+                if not os.path.isdir(os.path.join(path, "LC_MESSAGES")):
+                    continue
+                code = name.replace("_", "-").lower()
+                if code not in language_dict:
+                    name = _(get_locale_name(code))
+                    language_dict[code] = name
+    
+        LANGUAGES = sorted(language_dict.items())
+    
+    
+    API_DOC_INCLUDES = {}
+    API_DOC_PATH = os.path.join(BASE_DIR, "docs", "api")
+    for i, j, files in os.walk(API_DOC_PATH):
+        for file in files:
+            base, ext = os.path.splitext(file)
+            if ext == ".md":
+                API_DOC_INCLUDES[base] = os.path.join(API_DOC_PATH, file)
+    
+
