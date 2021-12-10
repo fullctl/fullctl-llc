a33d68498f63b18ea6367a993e9dbad97c47e39c
Stefan Pratter <stefan@20c.com>
2021-12-10 08:49:54 +0000

    allow fullctl addons to set up urls autonomously

--- src/fullctl/django/settings/default.py
+++ src/fullctl/django/settings/default.py
@@ -87,3 +87,5 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
+
+FULLCTL_ADDON_URLS = []
