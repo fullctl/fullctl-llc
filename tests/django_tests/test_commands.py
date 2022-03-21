5672c4d3703d910f5b6c34521ed055106ed75798
Jason Carvalho <jason@20c.com>
2022-03-21 10:25:23 +0530

    add test_fullctl_work_task assertions

--- tests/django_tests/test_commands.py
+++ tests/django_tests/test_commands.py
@@ -2,6 +2,10 @@ import pytest
 from django.contrib.auth import get_user_model
 from django.core import management
 
+import tests.django_tests.testapp.models as models
+from fullctl.django.models import Task
+from fullctl.django.tasks.orm import specify_task
+
 
 def test_fullctl_peeringdb_sync(db, dj_account_objects):
     with pytest.raises(SystemExit):
@@ -23,5 +27,9 @@ def test_fullctl_promote_user(db, dj_account_objects_c):
 
 
 def test_fullctl_work_task(db, dj_account_objects):
-    with pytest.raises(SystemExit):
-        management.call_command("fullctl_work_task", "--help")
+    task = models.TestTask.create_task(1, 2)
+    management.call_command("fullctl_work_task", "1")
+    task = specify_task(Task.objects.get(id=1))
+
+    assert task.status == "completed"
+    assert int(task.output) == 3
