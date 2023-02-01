0f756195f97886d8d11f09b21eb63d7a3f71dbaf
Stefan Pratter <stefan@20c.com>
2023-02-01 17:32:05 +0000

    linting

--- src/fullctl/django/migrations/0026_auto_20230131_1405.py
+++ src/fullctl/django/migrations/0026_auto_20230131_1405.py
@@ -6,20 +6,25 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('django_fullctl', '0025_auto_20221025_1215'),
+        ("django_fullctl", "0025_auto_20221025_1215"),
     ]
 
     operations = [
         migrations.AddIndex(
-            model_name='auditlog',
-            index=models.Index(fields=['org_id', 'action'], name='django_full_org_id_29fa6f_idx'),
+            model_name="auditlog",
+            index=models.Index(
+                fields=["org_id", "action"], name="django_full_org_id_29fa6f_idx"
+            ),
         ),
         migrations.AddIndex(
-            model_name='auditlog',
-            index=models.Index(fields=['user'], name='django_full_user_id_36d22e_idx'),
+            model_name="auditlog",
+            index=models.Index(fields=["user"], name="django_full_user_id_36d22e_idx"),
         ),
         migrations.AddIndex(
-            model_name='auditlog',
-            index=models.Index(fields=['object_type', 'object_id'], name='django_full_object__5cc546_idx'),
+            model_name="auditlog",
+            index=models.Index(
+                fields=["object_type", "object_id"],
+                name="django_full_object__5cc546_idx",
+            ),
         ),
     ]
