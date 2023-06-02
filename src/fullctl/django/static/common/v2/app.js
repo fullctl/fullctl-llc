f9ed9e8aa8e19f6ed105136163536a0233e6a079
Jason Carvalho <jason@20c.com>
2023-06-02 18:43:47 +0000

    add fullctl.formatters.seconds_to_wdhms

--- src/fullctl/django/static/common/v2/app.js
+++ src/fullctl/django/static/common/v2/app.js
@@ -189,6 +189,28 @@ fullctl.formatters.datetime = (value) => {
 }
 
 /**
+ * Formats seconds as XwXd if its higher than 48 hours
+ * and hh:mm:ss otherwise
+ */
+fullctl.formatters.seconds_to_wdhms = (seconds) => {
+  seconds = Number(seconds);
+  const w = Math.floor(seconds / (3600*24*7));
+  const d = Math.floor(seconds % (3600*24*7) / (3600*24));
+
+  const wDisplay = w > 0 ? w + "w" : "";
+  const dDisplay = d > 0 ? d + "d" : "";
+  if (w>0 || d>2) {
+    return (wDisplay + dDisplay).replace(/,\s*$/, "");
+  }
+
+  const h = Math.floor(seconds / 3600);
+  const m = Math.floor(seconds % 3600 / 60);
+  const s = Math.floor(seconds % 60);
+
+  return (`${h}:${m}:${s}`);
+}
+
+/**
  * Formats `True` and `False` as checkmark and cross
  * @method pretty_bool
  * @param {String} value value of cell
