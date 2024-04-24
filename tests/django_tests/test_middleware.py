0fc4af603746faea31a88412b3a59a952aacc53a
Jason Carvalho <jason@20c.com>
2024-04-24 17:37:48 +0000

    add tests for AutocompleteRequestPermsMiddleware

---
+++ tests/django_tests/test_middleware.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.test import RequestFactory, SimpleTestCase
+
+from fullctl.django.middleware import AutocompleteRequestPermsMiddleware
+
+
+def get_response_empty(request):
+    return HttpResponse()
+
+
+class AutocompleteRequestPermsMiddlewareTest(SimpleTestCase):
+    rf = RequestFactory()
+
+    def test_non_autocomplete_path(self):
+        request = self.rf.get("/path/", HTTP_AUTHORIZATION="Bearer test")
+        r = AutocompleteRequestPermsMiddleware(get_response_empty).process_request(
+            request
+        )
+        # asert an attribute error using pytest
+        with self.assertRaises(AttributeError):
+            r.api_key
+        self.assertEqual(r.perms, None)
