2de5fc84d104d92278b9745784c5989a13123bbf
Stefan Pratter <stefan@20c.com>
2024-06-25 10:51:38 +0000

    impersonation allowed for users with explicit permissions to impersonate

--- src/fullctl/django/admin.py
+++ src/fullctl/django/admin.py
@@ -219,14 +219,28 @@ class UrlActionMixin:
         Allows one to call any actions defined in this model admin
         to be called via an admin view placed at <model_name>/<id>/<action>/<action_name>.
         """
-        if not request.user.is_superuser:
-            return HttpResponseForbidden(request)
-
         obj = self.get_queryset(request).filter(pk=object_id)
         if obj.exists():
             redir = self.make_redirect(obj, action)
             action = self.get_action(action)
             if action:
+                
+                # action function found, next check if the user has
+                # permission to perform the action
+                permissions = action[0].allowed_permissions
+                allowed = False
+
+                # if the user passes any of the permissions, allow the action
+                for perm in permissions:
+                    fn_check_perm  = getattr(self, f"has_{perm}_permission")
+                    allowed = fn_check_perm(request)
+                    if allowed:
+                        break
+                
+                if not allowed:
+                    # user does not have permission to perform the action
+                    return HttpResponseForbidden("You do not have permission to perform this action")
+
                 action[0](self, request, obj)
                 return redir
         return redirect(
